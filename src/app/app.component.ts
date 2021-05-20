3c6455e0c0edb1a4e943501fffa1cefd7e5d5583
Anmol <anmolj2009@gmail.com>
2021-05-20 14:27:30 +0530

    drag & drop task list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 import { Task } from './task/task';
 
@@ -18,4 +19,22 @@ export class AppComponent {
       description: "Write tech article on Angular Dev Tools"
     }
   ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void {}
+    drop(event: CdkDragDrop<Task[] | null>): void {
+      if (event.previousContainer === event.container) {
+        return;
+      }
+      if (!event.container.data || !event.previousContainer.data) {
+        return;
+      }
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
 }
