e0533a2f08c5d158694292fcc41848d5621c502a
Anmol <anmolj2009@gmail.com>
2021-05-23 16:30:44 +0530

    firebase app deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyBKtfzVEZi9bUC_wdpDEPfXNBlifI0RLEQ",
+    authDomain: "kanbanfire-3e3ed.firebaseapp.com",
+    projectId: "kanbanfire-3e3ed",
+    storageBucket: "kanbanfire-3e3ed.appspot.com",
+    messagingSenderId: "600529840060",
+    appId: "1:600529840060:web:6e30dac03e0a9b840d2db9",
+    measurementId: "G-7WYSKN9S2M"
+  }
+  
 };
